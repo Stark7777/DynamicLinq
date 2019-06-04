035b4a3b2e7c71f6d64b4ac74f2c0daf31210eab
Jim <jimmyleiva07@gmail.com>
2019-06-04 15:35:46 -0600

    Modelo de Base de Datos

--- LinDynamic/LinDynamic/Program.cs
+++ LinDynamic/LinDynamic/Program.cs
@@ -18,7 +18,24 @@ namespace LinDynamic
         }
         static void Main(string[] args)
         {
+            //Instancia al contexto de datos
+            Nomina_SAEntities contexto = new Nomina_SAEntities();
+            //Use IQueryable LazyLoading
+            IQueryable<Empleados> ListaEmpelados = contexto.Empleados.Where(e => e.SalarioMensual > 700);
+            ListaEmpelados = ListaEmpelados.Take(10);
 
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine("---------------Lista de Empleados Financiera Fundeser");
+
+            foreach (Empleados e in ListaEmpelados)
+            {
+                Console.WriteLine("Codigo {0}", e.CodigoEmpleado);
+                Console.WriteLine("Nombre {0}", e.Nombre);
+                Console.WriteLine("Salario en ${0}", e.SalarioMensual);
+
+                Console.WriteLine("###################################");
+            }
+          
         }
     }
 }
