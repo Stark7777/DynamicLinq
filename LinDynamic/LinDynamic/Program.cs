2a67869580e484019a447a59f0ce229c2ee787ec
Jim <jimmyleiva07@gmail.com>
2019-06-04 15:12:33 -0600

    Nueva Clase Empleado

--- LinDynamic/LinDynamic/Program.cs
+++ LinDynamic/LinDynamic/Program.cs
@@ -8,8 +8,17 @@ namespace LinDynamic
 {
     class Program
     {
+        class Empleado
+        {
+            public string CodigoEmpleado { get; set; }
+            public string Nombre { get; set; }
+            public string Apelido { get; set; }
+            public string Cedula { get; set; }
+            public string Direccion { get; set; }
+        }
         static void Main(string[] args)
         {
+
         }
     }
 }
